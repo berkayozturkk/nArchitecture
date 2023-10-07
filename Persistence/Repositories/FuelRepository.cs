14c8a021bf0c6165dd92e61b561307ab415f8886
Berkay Öztürk <73599896+berkayozturkk@users.noreply.github.com>
2023-10-07 17:44:23 +0300

    Repository implamantation

---
+++ Persistence/Repositories/FuelRepository.cs
@@ -0,0 +1,15 @@
+﻿using Application.Services.Repositories;
+using Core.Persistence.Repositories;
+using Domain.Entities;
+using Persistence.Context;
+
+namespace Persistence.Repositories;
+
+public class FuelRepository
+    : EfRepositoryBase<Fuel, Guid, BaseDbContext>, IFuelRepository
+{
+    public FuelRepository(BaseDbContext context) : base(context)
+    {
+
+    }
+}
